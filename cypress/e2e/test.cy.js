4653dc9236c98806910523987b40decbfaeb4e17
unknown <pratikh73@gmail.com>
2024-05-26 18:27:42 +0530

    first commit

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,7 @@
+describe('Demo test', () => {
+    it('sample test', () => {
+      cy.visit('https://www.google.com/')
+      cy.get('.SDkEP').type('Testing{Enter}')
+    //   cy.get('.FPdoLc > center > .gNO89b').click()
+    })
+  })
